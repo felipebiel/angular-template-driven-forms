fec0823a5a8d86c7c21b2b56e0f6687543a57da8
Felipe Biel <felipe.biel@t10.digital>
2024-09-09 19:05:30 -0300

    feat: Serviço base de estados basileiros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GenresService } from './services/genres.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,23 @@ import { GenresService } from './services/genres.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   genresList: any = [];
+  statesList: any = [];
   constructor(
     private readonly _usersServices: UsersService,
     private readonly _genresService: GenresService,
+    private readonly _BrazilianStatesService: BrazilianStatesService,
   ) {}
 
   ngOnInit(): void {
     this.getUsers();
     this.getGenres();
+    this.getStates();
+  }
+  private getStates() {
+    this._BrazilianStatesService.getStates().subscribe((statesListResponse) => {
+      console.log(statesListResponse);
+      this.statesList = statesListResponse;
+    });
   }
 
   private getGenres() {
