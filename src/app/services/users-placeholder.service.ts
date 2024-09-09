79603d8782724fca32a6fe63c158f15ba3a93820
Felipe Biel <felipe.biel@t10.digital>
2024-09-09 19:14:14 -0300

    feat: Serviço base de json placeholder

---
+++ src/app/services/users-placeholder.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersPlaceholderService {
+  private url: string = 'https://jsonplaceholder.typicode.com/users';
+  constructor(private readonly _httpClient: HttpClient) {}
+
+  getusersPlaceholder(): Observable<any> {
+    return this._httpClient.get(this.url);
+  }
+}
