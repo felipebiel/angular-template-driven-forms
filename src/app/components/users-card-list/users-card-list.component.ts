d369407520bb60e3c08fd0ff59eed13fad59f727
Felipe Biel <felipe.biel@t10.digital>
2024-09-10 14:48:15 -0300

    feat: Passando o usersList para o componente de card-list

--- src/app/components/users-card-list/users-card-list.component.ts
+++ src/app/components/users-card-list/users-card-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersListResponse } from '../../types/users-list-reponse';
 
 @Component({
   selector: 'app-users-card-list',
   templateUrl: './users-card-list.component.html',
-  styleUrl: './users-card-list.component.scss'
+  styleUrl: './users-card-list.component.scss',
 })
 export class UsersCardListComponent {
-
+  @Input() usersList: UsersListResponse = [];
 }
