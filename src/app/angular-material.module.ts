111a9b73d03bdb0ca3f0de816de26b015f1ea08f
Felipe Biel <felipe.biel@t10.digital>
2024-09-11 09:49:52 -0300

    feat: Campo de nome do usuário

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [MatCardModule],
-  exports: [MatCardModule],
+  imports: [MatCardModule, MatInputModule, MatFormFieldModule],
+  exports: [MatCardModule, MatInputModule, MatFormFieldModule],
 })
 export class AngularMaterialModule {}
--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { GenresListResponse } from '../../types/genres-list-response';
+import { StatesListResponse } from '../../types/states-list-response';
+import { IUser } from '../../interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
+  styleUrl: './user-form.component.scss',
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnChanges {
+  @Input() genresList: GenresListResponse = [];
+  @Input() statesList: StatesListResponse = [];
+  @Input() userSelected: IUser = {} as IUser;
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  }
 }
